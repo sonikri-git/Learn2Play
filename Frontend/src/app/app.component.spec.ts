ed2660f2e0511c078fc8854961b5aca05cfd9b67
Kritish Soni <sonikri@sheridancollege.ca>
2026-06-30 12:05:12 -0400

    Delete Frontend directory

--- Frontend/src/app/app.component.spec.ts
+++
@@ -1,7 +0,0 @@
-import { AppComponent } from './app.component';
-
-describe('AppComponent', () => {
-  it('should create', () => {
-    expect(true).toBe(true);
-  });
-});
--- Frontend/src/app/core/auth/token-storage.ts
+++
@@ -1,5 +0,0 @@
-export const TokenStorage = {
-  get: () => localStorage.getItem('accessToken'),
-  set: (t: string) => localStorage.setItem('accessToken', t),
-  clear: () => localStorage.removeItem('accessToken'),
-};
