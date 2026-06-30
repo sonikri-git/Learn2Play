c8ac901cf6dd3c707fdeb5a6e8fa7de09fe63a0a
Kritish Soni <sonikri@sheridancollege.ca>
2026-06-30 12:08:55 -0400

    Delete Backend directory

--- Backend/src/main/java/com/learn2play/backend/repository/UserAnswerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.learn2play.backend.repository;
-
-import com.learn2play.backend.entity.UserAnswer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserAnswerRepository extends JpaRepository<UserAnswer, Long> {
-}
